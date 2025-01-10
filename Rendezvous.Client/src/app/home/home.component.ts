997f7b7898a5d8967391bfd62d823bd75b206ced
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-01-10 15:57:29 +0100

    Add login and register to client

---
+++ Rendezvous.Client/src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  http = inject(HttpClient);
+  registerMode = false;
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:7147/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.error(error),
+      complete: () => console.log('Request has completed.'),
+    });
+  }
+}
