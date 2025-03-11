379e6e644568f107cb90c33a866f4604c9183853
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-03-11 17:51:28 +0100

    Add roles

--- Rendezvous.API/Data/Seed.cs
+++ Rendezvous.API/Data/Seed.cs
@@ -7,7 +7,8 @@ namespace Rendezvous.API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager,
+        RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync())
         {
@@ -24,10 +25,34 @@ public class Seed
             return;
         }
 
+        var roles = new List<AppRole>
+        {
+            new() { Name = "Member" },
+            new() { Name = "Admin" },
+            new() { Name = "Moderator" }
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+
         foreach (var user in users)
         {
             user.UserName = user.UserName!.ToLower();
             await userManager.CreateAsync(user, "P@$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin = new AppUser
+        {
+            UserName = "admin",
+            KnownAs = "Admin",
+            Gender = string.Empty,
+            City = string.Empty,
+            Country = string.Empty
+        };
+        await userManager.CreateAsync(admin, "P@$$w0rd");
+        await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
     }
 }
