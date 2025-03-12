78513ad588aad979ddb9033ce85c2437309ba572
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-03-12 11:36:47 +0100

    Implement getting users with roles

--- Rendezvous.API/Controllers/AdminController.cs
+++ Rendezvous.API/Controllers/AdminController.cs
@@ -1,15 +1,27 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rendezvous.API.Entities;
 
 namespace Rendezvous.API.Controllers;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRolesAsync()
     {
-        return Ok("Only admins can see this");
+        var users = await userManager.Users
+            .OrderBy(u => u.UserName)
+            .Select(u => new
+            {
+                u.Id,
+                Username = u.UserName,
+                Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
+
+        return Ok(users);
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
