da3870ffdfafe63774f09af43ba0778c1617e5bb
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-03-13 14:56:29 +0100

    Fix typo

--- Rendezvous.API/SignalR/PresenceHub.cs
+++ Rendezvous.API/SignalR/PresenceHub.cs
@@ -14,7 +14,7 @@ public class PresenceHub : Hub
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        await Clients.Others.SendAsync("UserIfOffline", Context.User?.GetUsername());
+        await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUsername());
         await base.OnDisconnectedAsync(exception);
     }
 }
