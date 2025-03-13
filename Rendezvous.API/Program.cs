1c417bca036522c7ac3b435795e5f6cfb3c75434
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-03-13 22:05:53 +0100

    Create message hub

--- Rendezvous.API/Program.cs
+++ Rendezvous.API/Program.cs
@@ -27,6 +27,7 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
---
+++ Rendezvous.API/SignalR/MessageHub.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.SignalR;
+using Rendezvous.API.Extensions;
+using Rendezvous.API.Interfaces;
+
+namespace Rendezvous.API.SignalR;
+
+public class MessageHub(IMessageRepository messageRepository) : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        var otherUser = httpContext?.Request.Query["user"];
+
+        if (Context.User == null || string.IsNullOrEmpty(otherUser))
+        {
+            throw new Exception("Cannot join group.");
+        }
+
+        var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        var messages = await messageRepository
+            .GetMessageThreadAsync(Context.User.GetUsername(), otherUser!);
+        await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private string GetGroupName(string caller, string? other)
+    {
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}
